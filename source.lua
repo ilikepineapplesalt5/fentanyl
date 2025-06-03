0312aa6389f3163f8d201020f9f9c147e9768f70
ilikepineapplesalt5 <137695587+ilikepineapplesalt5@users.noreply.github.com>
2025-06-03 18:04:18 +0200

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,32 @@
+local placeId = game.PlaceId
+
+--/ Crash Out Simulator
+if placeId == 15839834917 then
+    print("Crash Out Simulator")
+loadstring(game:HttpGet("https://pastefy.app/6j0CmXgN/raw", true))()
+
+
+--/ Cash Incremental
+elseif placeId == 129159449618378 then
+    print("Cash Incremental")
+loadstring(game:HttpGet("https://pastefy.app/cxkvygky/raw", true))()
+
+
+--/ Untitled Drill Game
+elseif placeId == 87700573492940 then
+    print("Untitled Drill Game")
+loadstring(game:HttpGet("https://pastefy.app/dL1HWV28/raw", true))()
+
+--/ Factory RNG
+elseif placeId == 76381016848158 then
+    print("Factory RNG")
+loadstring(game:HttpGet("https://pastefy.app/K0zB6dzl/raw", true))()
+  
+  --/ Egg Incremental
+elseif placeId == 137034315542002 then
+    print("Egg Incremental")
+loadstring(game:HttpGet("https://pastefy.app/ismiGrdU/raw", true))()
+
+else
+    print("No Script Found For This Games")
+end
